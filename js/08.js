a5f6d26998f69850a7024e2b36bfcacf24a23bab
Andres-GMP <al20760260@ite.edu.mx>
2022-03-23 11:22:08 -0700

    nuevos documentos de js con pruebas

---
+++ js/08.js
@@ -0,0 +1,14 @@
+// Uso de arreglos: Crear arreglo
+const numero = [10,20,30,40,50,60]
+
+// Recorrer el arreglo con una función normal
+console.log('Recorrido con funcion')
+// forEach: Para cada elemento
+
+numero.forEach(function(value){
+    console.log(value)
+   
+})
+ console.log("Hasta aquí imprime uno\n")
+
+numero.forEach(num => console.log(num))
